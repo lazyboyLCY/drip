186b41c211300e9679131a58fe4d31638964ec45
lichenyang <1003737038@qq.com>
2020-05-30 19:23:52 +0800

    新增文章模块页面

--- src/main/java/com/li/drip/controller/PublicArticleController.java
+++ src/main/java/com/li/drip/controller/PublicArticleController.java
@@ -1,4 +1,28 @@
 package com.li.drip.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/publicarticle")
 public class PublicArticleController {
+    @RequestMapping("/testpublicarticle")
+    public ModelAndView publicarticle(HttpServletRequest request) {
+        ModelAndView mv = new ModelAndView("publicarticle");
+        String username= (String) request.getSession().getAttribute("username");
+        mv.addObject("username",username);
+        return mv;
+    }
+    @RequestMapping("/myarticle")
+    public ModelAndView myarticle(HttpServletRequest request) {
+        ModelAndView mv = new ModelAndView("myarticle");
+        String username= (String) request.getSession().getAttribute("username");
+        mv.addObject("username",username);
+        return mv;
+    }
+
+
 }
